a42e27731a6ef8684c4d50dae3fc184fe335f887
stricker <liuchen195@163.com>
2016-10-20 13:54:08 +0800

    Initial Commit

---
+++ bameng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bameng
+//
+//  Created by 刘琛 on 16/10/20.
+//  Copyright © 2016年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bameng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bameng
+//
+//  Created by 刘琛 on 16/10/20.
+//  Copyright © 2016年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
