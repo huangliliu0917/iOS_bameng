65a5f1b1dc7a5e3f76d5806f40b10b310c655e4c
sxluohaibo@163.com <605455337@qq.com>
2016-11-29 09:37:07 +0800

    测试

--- bameng/Class/盟主/盟主TAB/PushWebViewController.m
+++ bameng/Class/盟主/盟主TAB/PushWebViewController.m
@@ -9,7 +9,9 @@
 #import "PushWebViewController.h"
 #import "AppDelegate.h"
 #import <ShareSDKUI/ShareSDK+SSUI.h>
-
+#import <ShareSDKUI/SSUIShareActionSheetStyle.h>
+//#自定义分享编辑界面所需要导入的头文件
+#import <ShareSDKUI/SSUIEditorViewStyle.h>
 
 @interface PushWebViewController ()<WKUIDelegate,WKNavigationDelegate>
 
@@ -65,6 +67,7 @@
                                           title:[shareArray firstObject]
                                            type:SSDKContentTypeAuto];
         //2、分享（可以弹出我们的分享菜单和编辑界面）
+        [SSUIShareActionSheetStyle setShareActionSheetStyle:ShareActionSheetStyleSimple];
         [ShareSDK showShareActionSheet:nil //要显示菜单的视图, iPad版中此参数作为弹出菜单的参照视图，只有传这个才可以弹出我们的分享菜单，可以传分享的按钮对象或者自己创建小的view 对象，iPhone可以传nil不会影响
                                  items:nil
                            shareParams:shareParams
